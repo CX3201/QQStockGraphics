39908987822449d289b8b93ef94296ac49709fe6
abelchen(陈曦) <abelchen@tencent.com>
2018-07-02 16:03:44 +0800

    添加文件

---
+++ QQStockGraphics/Sources/Common/QSGCommon.swift
@@ -0,0 +1,14 @@
+//
+//  QSGCommon.swift
+//  QQStockGraphics
+//
+//  Created by abelchen on 2018/6/21.
+//  Copyright © 2018年 abelchen. All rights reserved.
+//
+
+import Foundation
+
+enum QSGStockType {
+    case HSNoraml
+    case HSIndex
+}
