283f4405f9f145bf6a55f3f50d70bba7c0f222d8
Abelchen <cxxhcj@gmail.com>
2018-07-11 11:29:05 +0800

    推进

--- QQStockGraphics/Sources/Graph/QSGCanvas.swift
+++ QQStockGraphics/Sources/Graph/QSGCanvas.swift
@@ -8,7 +8,32 @@
 
 import UIKit
 
+typealias QSGColor = UInt32
+
+extension QSGColor {
+    var toUIColor: UIColor {
+        get {
+            let r = CGFloat((self & 0xFF000000) >> 24) / 256
+            let g = CGFloat((self & 0x00FF0000) >> 16) / 256
+            let b = CGFloat((self & 0x0000FF00) >> 8) / 256
+            let a = CGFloat((self & 0x000000FF) >> 0) / 256
+            
+            return UIColor(red: r, green: g, blue: b, alpha: a)
+        }
+    }
+}
+
+struct QSGPoint {
+    let x: QSGValue
+    let y: QSGValue
+    let color: QSGColor
+}
+
 class QSGCanvas : CALayer {
     var color: UIColor = UIColor.black
     var lineWidth: Double = 1;
+}
+
+class QSGUniformCanvas : QSGCanvas {
+    
 }
